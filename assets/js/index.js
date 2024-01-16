6a498b4c70e82bb597cf983754c8276ea1a3a28d
RafaelContact <contactrodrigues21@gmail.com>
2024-01-16 03:00:41 -0300

    commit com vários erros de formatação"

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+// 1 colocar as letras do titulo em upper case
+// fazer que ao recarregar a pagina o textarea receba valor nulo
+//colocar um prevent deffaut para quando o número não estiver preenchido com uma mensagem de preencha o número de whatssapp
