d26aafdff2ea86174874a741d196c948c900768a
RafaelContact <contactrodrigues21@gmail.com>
2024-01-17 02:10:57 -0300

    add limit of div_warning click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,30 @@
 // inserir mensagem de erro caso clique no gerar link
-
-let area = document.querySelector('.msg').innerHTML = ""
-
 let form = document.querySelector('.form')
 let get_link = document.querySelector('.sub_btn')
 let coppy_link = document.querySelector('.coppy_btn')
 
 let tel = document.querySelector('.tel')
+let div_warning = document.querySelector('.warning')
 
 
-form.addEventListener('submit', doEvent);
+
+let area = document.querySelector('.msg').innerHTML = ""
+
+form.addEventListener('submit', doEvent)
 function doEvent(e){
     console.log(`ipo do evento: ${e.type}`)
     e.preventDefault()
 }
-const sub_warning = document.createElement("p")
-const warning = document.createTextNode("Digite o número do whatssapp")
-sub_warning.appendChild(warning)
 
-.appendChild(sub_warning)
+get_link.addEventListener('click', warning)
+
+function warning(){
+        const sub_warning = document.createElement("p")
+        const warning = document.createTextNode("Digite o número do whatssapp!")
+        sub_warning.appendChild(warning)
+        let cont = document.querySelectorAll('.warning p').length
+        console.log(cont)
+            if( cont === 0){
+                div_warning.appendChild(sub_warning)
+        }
+}
